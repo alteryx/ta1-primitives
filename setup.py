c922b94b1e6e1269f0080a8168956878721c6e38
bschreck <benjaminschreck93@gmail.com>
2018-06-04 12:51:03 -0700

    bumped version, using specific d3m version of ft still

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     author=read_package_variable('__author__'),
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     install_requires=[
-        'featuretools==0.1.21',
+        'featuretools==0.1.21.d3m.2018.4.18',
         "sklearn_wrap==0.1.1",
         'd3m==v2018.4.18',
         'typing'
@@ -41,7 +41,7 @@ setup(
     url='https://gitlab.datadrivendiscovery.org/MIT-FeatureLabs/ta1-primitives',
     dependency_links=[
         "git+https://gitlab.com/datadrivendiscovery/sklearn-wrap.git@86d94c5859f9567eaf2b7dad180c204e7a9ac25d#egg=sklearn_wrap-0.1.1",
-        'git+https://github.com/Featuretools/featuretools.git@v0.1.21.d3m.2018.4.18#egg=featuretools-0.1.21',
+        'git+https://github.com/Featuretools/featuretools.git@v0.1.21.d3m.2018.4.18#egg=featuretools-0.1.21.d3m.2018.4.18',
     ],
     entry_points={
         'd3m.primitives': [
