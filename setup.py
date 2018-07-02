8c6fb28c9630e86c3307aa781c57b07bc0bf2d78
bschreck <benjaminschreck93@gmail.com>
2018-07-02 12:30:27 -0600

    bump ft version

--- setup.py
+++ setup.py
@@ -33,13 +33,13 @@ setup(
     author=read_package_variable('__author__'),
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     install_requires=[
-        'featuretools==0.1.21.d3m.2018.4.18',
+        'featuretools==0.2.0',
         'd3m==v2018.6.5',
         'typing'
     ],
     url='https://gitlab.datadrivendiscovery.org/MIT-FeatureLabs/ta1-primitives',
     dependency_links=[
-        'git+https://github.com/Featuretools/featuretools.git@v0.1.21.d3m.2018.4.18#egg=featuretools-0.1.21.d3m.2018.4.18',
+        'git+https://github.com/Featuretools/featuretools.git@v0.2.0#egg=featuretools-0.2.0'
     ],
     entry_points={
         'd3m.primitives': [
