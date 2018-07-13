d9a3ddba4bbff45bad1c411758576b6496cdf649
Carles Sala <c.sala.stq@gmail.com>
2018-07-13 19:21:56 +0200

    Fixes

--- save_primitive_jsons.sh
+++
@@ -1,4 +0,0 @@
-python -m d3m.index describe -i 4 d3m.primitives.featuretools_ta1.RFRegressorFeatureSelector > ../primitives_repo/v2018.4.18/MIT_FeatureLabs/d3m.primitives.featuretools_ta1.RFRegressorFeatureSelector/0.2.0/primitive.json
-python -m d3m.index describe -i 4 d3m.primitives.featuretools_ta1.RFClassifierFeatureSelector > ../primitives_repo/v2018.4.18/MIT_FeatureLabs/d3m.primitives.featuretools_ta1.RFClassifierFeatureSelector/0.2.0/primitive.json
-python -m d3m.index describe -i 4 d3m.primitives.featuretools_ta1.Imputer > ../primitives_repo/v2018.4.18/MIT_FeatureLabs/d3m.primitives.featuretools_ta1.Imputer/0.2.0/primitive.json
-python -m d3m.index describe -i 4 d3m.primitives.featuretools_ta1.DFS > ../primitives_repo/v2018.4.18/MIT_FeatureLabs/d3m.primitives.featuretools_ta1.DFS/0.2.0/primitive.json
