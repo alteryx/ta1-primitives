07d9a6360b4684a9c5a285b321e0403d73a9cbed
Max Kanter <kmax12@gmail.com>
2019-05-31 23:15:49 -0400

    update

--- featuretools_ta1/config.py
+++ featuretools_ta1/config.py
@@ -4,7 +4,16 @@ import os
 
 AUTHOR = "MIT_FeatureLabs"
 CONTACT = "mailto:max.kanter@featurelabs.com"
-INSTALLATION = [{
+
+try:
+    INSTALLATION = [{
+                   'type': metadata_base.PrimitiveInstallationType.PIP,
+                   'package_uri': 'git+https://github.com/Featuretools/ta1-primitives.git@{git_commit}#egg=featuretools_ta1'.format(
+                       git_commit=d3m_utils.current_git_commit(d3m_utils.current_git_commit(os.path.dirname(__file__))),
+                   ),
+                }]
+except:
+    INSTALLATION = [{
                'type': metadata_base.PrimitiveInstallationType.PIP,
                'package_uri': 'git+https://github.com/Featuretools/ta1-primitives.git@{git_commit}#egg=featuretools_ta1'.format(
                    git_commit=d3m_utils.current_git_commit("/ta1-primitives"),
