ff53665498e2ff6a60a5e708e300ca311a183d64
Max Kanter <kmax12@gmail.com>
2019-05-31 23:29:34 -0400

    updates

--- featuretools_ta1/config.py
+++ featuretools_ta1/config.py
@@ -1,17 +1,15 @@
 from d3m import utils as d3m_utils
 from d3m.metadata import base as metadata_base
 import os
+from pathlib import Path
+
 
 AUTHOR = "MIT_FeatureLabs"
 CONTACT = "mailto:max.kanter@featurelabs.com"
 
-try:
-    _git_commit = d3m_utils.current_git_commit("/ta1-primitives")
 
-except:
-
-    _git_commit = d3m_utils.current_git_commit(d3m_utils.current_git_commit(os.path.dirname(__file__)))
-
+_git_commit = d3m_utils.current_git_commit(Path(__file__).parents[1])
+print(_git_commit)
 INSTALLATION = [{
                'type': metadata_base.PrimitiveInstallationType.PIP,
                'package_uri': 'git+https://github.com/Featuretools/ta1-primitives.git@{git_commit}#egg=featuretools_ta1'.format(
