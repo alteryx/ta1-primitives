cf863e7fa86de157e0866430c2965edea55bfad6
Max Kanter <kmax12@gmail.com>
2019-06-15 14:36:45 -0400

    updates

--- singletable_test_1.py
+++ singletable_test_1.py
@@ -36,23 +36,28 @@ step_3.add_hyperparameter(name='use_semantic_types', argument_type=ArgumentType.
 step_3.add_output('produce')
 pipeline_description.add_step(step_3)
 
-
-# Step 4: learn model
-step_4 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.regression.xgboost_gbtree.DataFrameCommon'))
-step_4.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.3.produce')
-step_4.add_argument(name='outputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.1.produce')
+step_4 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.one_hot_encoder.DistilOneHotEncoder'))
+step_4.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference="steps.3.produce")
 step_4.add_output('produce')
 pipeline_description.add_step(step_4)
 
-# step 5: construct output
-step_5 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.construct_predictions.DataFrameCommon'))
+
+# Step 4: learn model
+step_5 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.classification.random_forest.SKlearn'))
 step_5.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.4.produce')
-step_5.add_argument(name='reference', argument_type=ArgumentType.CONTAINER, data_reference='steps.1.produce')
+step_5.add_argument(name='outputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.1.produce')
 step_5.add_output('produce')
 pipeline_description.add_step(step_5)
 
+# step 5: construct output
+step_6 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.construct_predictions.DataFrameCommon'))
+step_6.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.5.produce')
+step_6.add_argument(name='reference', argument_type=ArgumentType.CONTAINER, data_reference='steps.1.produce')
+step_6.add_output('produce')
+pipeline_description.add_step(step_6)
+
 # Final Output
-pipeline_description.add_output(name='output predictions', data_reference='steps.5.produce')
+pipeline_description.add_output(name='output predictions', data_reference='steps.6.produce')
 
 # Output to YAML
 # print(pipeline_description.to_yaml())
