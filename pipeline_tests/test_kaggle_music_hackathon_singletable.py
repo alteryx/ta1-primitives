2ce62b07789d523424b2adac3f69751e20b3f38e
Nate Parsons <nate.parsons@alteryx.com>
2019-11-22 16:04:39 -0600

    fix kaggle test output

--- pipeline_tests/test_kaggle_music_hackathon_singletable.py
+++ pipeline_tests/test_kaggle_music_hackathon_singletable.py
@@ -74,7 +74,6 @@ def generate_only():
     fs_cmd += ' -t {}/{}/TEST/dataset_TEST/datasetDoc.json'.format(dataset_path, dataset_name)
     fs_cmd += ' -a {}/{}/SCORE/dataset_TEST/datasetDoc.json'.format(dataset_path, dataset_name)
     fs_cmd += ' -O {}'.format(pipeline_run_file)
-    fs_cmd += ' --output kaggle_single.csv'
 
     # Run pipeline to save pipeline_run file
     os.system(fs_cmd)
